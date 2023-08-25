c4239fb9e4673118998ee41a65ceb2372c577dd0
mayarapreta <mayaradornelesd@gmail.com>
2023-08-25 13:09:39 -0300

    Conta-banco-feito-desafio

--- Conta-banco/src/Contaterminal.java
+++ Conta-banco/src/Contaterminal.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Contaterminal {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    
+		System.out.println("***BANCO DO BRASIL***"); //imprimir a saida do inicio
+
+	
+		    Scanner input = new Scanner(System.in); // começa da compilacao
+		System.out.println("Por favor, digite o número da Agência !" );
+
+		String textNameString = input.nextLine();   // string input agencia
+System.out.println("sua agencia é: " + textNameString );
+
+String nome_cliente = input.nextLine();
+		nome_cliente = "MARIO ANDRADE"; 
+		System.out.println("Usuario: " + nome_cliente );
+		
+	System.out.println("Inserir sua conta:  ");	 
+int numconta = input.nextInt();
+
+System.out.println("Inserir o valor do seu saldo:");
+		double saldonum = input.nextDouble();
+			input.close();
+
+	System.out.println("ÓLA CLIENTE ORBIAGDO POR CRIAR UMA CONTA EM NOSSO BANCO, SUA AGENCIA É: "+ textNameString + 
+			" CONTA " + numconta+ "O SEU SALDO " + saldonum +
+			" JÁ ESTA DISPONIVEL PARA SAQUE");
+	
+			
+			
     }
 }
